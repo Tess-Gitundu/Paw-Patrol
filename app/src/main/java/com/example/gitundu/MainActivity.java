c18ef8dba8499e0587b9ca4bc0996b571a3a7ae2
Tess <njerigitundu003@gmail.com>
2022-05-31 12:28:14 +0300

    Add click listener to button

--- app/src/main/java/com/example/gitundu/MainActivity.java
+++ app/src/main/java/com/example/gitundu/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.gitundu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mSeeAllPetsButton; //a member variable to hold our seeAllPetsButton to access it inside all of our methods
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSeeAllPetsButton = (Button)findViewById(R.id.seeAllPetsButton);
+        mSeeAllPetsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Jumping to all pets", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
