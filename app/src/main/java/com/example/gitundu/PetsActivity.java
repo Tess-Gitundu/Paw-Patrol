422c631c89fced4fd9c1834122351f2748744573
Tess <njerigitundu003@gmail.com>
2022-06-02 14:25:17 +0300

    Display data gathered from user

--- app/src/main/java/com/example/gitundu/PetsActivity.java
+++ app/src/main/java/com/example/gitundu/PetsActivity.java
@@ -4,14 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class PetsActivity extends AppCompatActivity {
+    private TextView mFinderTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pets);
+
+        mFinderTextView = (TextView) findViewById(R.id.finderTextView);
+
         Intent intent = getIntent();
-        String location = intent.getStringExtra("finder");
+        String finder = intent.getStringExtra("finder");
+        mFinderTextView.setText("Here are all the pets you can adopt in your area: " + finder);
     }
 }
