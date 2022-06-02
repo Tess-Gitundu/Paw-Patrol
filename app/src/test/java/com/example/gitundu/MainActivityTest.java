a2fc2d5eaf32d1955d62558fbb012628870b4079
Tess <njerigitundu003@gmail.com>
2022-06-02 22:17:54 +0300

    Create readme file

--- app/src/test/java/com/example/gitundu/MainActivityTest.java
+++ app/src/test/java/com/example/gitundu/MainActivityTest.java
@@ -1,9 +1,15 @@
 package com.example.gitundu;
 
+import static org.junit.Assert.assertTrue;
+
+import android.widget.TextView;
+
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
+import org.w3c.dom.Text;
 
 @RunWith(RobolectricTestRunner.class)
 public class MainActivityTest {
@@ -15,5 +21,11 @@ public class MainActivityTest {
                 .create()
                 .resume()
                 .get();
+    }
+
+    @Test
+    public void validateTextViewContent() {
+        TextView headerTextView = activity.findViewById(R.id.headerTextView);
+        assertTrue("Paw Patrol".equals(headerTextView.getText().toString()));
     }
 }
