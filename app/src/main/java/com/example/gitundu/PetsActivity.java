1dc93379139ca7ca2d5dd2f06f503d837a53fb4a
Tess <njerigitundu003@gmail.com>
2022-06-02 14:57:08 +0300

    Initialize listview and implement arrayadapter

--- app/src/main/java/com/example/gitundu/PetsActivity.java
+++ app/src/main/java/com/example/gitundu/PetsActivity.java
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class PetsActivity extends AppCompatActivity {
     private TextView mFinderTextView;
+    private ListView mPetsListView;
     private String[] pets = new String[] {"Dogs", "Cats", "Ferrets", "Rabbits", "Rodents", "Hedgehog", "Tenrecs", "Fish", "Reptiles", "Amphibians"};
 
     @Override
@@ -15,7 +18,11 @@ public class PetsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pets);
 
+        mPetsListView = (ListView) findViewById(R.id.petsListView);
         mFinderTextView = (TextView) findViewById(R.id.finderTextView);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, pets);
+        mPetsListView.setAdapter(adapter);
 
         Intent intent = getIntent();
         String finder = intent.getStringExtra("finder");
