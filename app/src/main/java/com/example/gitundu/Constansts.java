b62127522dffea42c3d1a85f4253179662c51e0a
Tess <njerigitundu003@gmail.com>
2022-06-08 20:10:07 +0300

    Populate constants class

--- app/src/main/java/com/example/gitundu/Constansts.java
+++ app/src/main/java/com/example/gitundu/Constansts.java
@@ -1,5 +1,6 @@
 package com.example.gitundu;
 
 public class Constansts {
-
+    public static final String PetFinder_BASE_URL = "https://www.petfinder.com/developers/v2";
+    public static final String PETFINDER_API_KEY = BuildConfig.YELP_API_KEY;
 }
