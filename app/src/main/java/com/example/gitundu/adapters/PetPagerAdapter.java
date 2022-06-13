e449d17355c731c20bf3d232bddd2d36cd888f06
Tess <njerigitundu003@gmail.com>
2022-06-13 05:06:57 +0300

    Add code to connect data to fragment view

--- app/src/main/java/com/example/gitundu/adapters/PetPagerAdapter.java
+++ app/src/main/java/com/example/gitundu/adapters/PetPagerAdapter.java
@@ -1,4 +1,35 @@
 package com.example.gitundu.adapters;
 
-public class PetPagerAdapter {
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.example.gitundu.PetDetailFragment;
+import com.example.gitundu.models.Animal;
+
+import java.util.List;
+
+public class PetPagerAdapter extends FragmentPagerAdapter {
+    private List<Animal> mPets;
+
+    public PetPagerAdapter(@NonNull FragmentManager fm, int behavior, List<Animal> pets) {
+        super(fm, behavior);
+        mPets = pets;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return PetDetailFragment.newInstance(mPets.get(position));
+    }
+
+    @Override
+    public int getCount() {
+        return mPets.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mPets.get(position).getName();
+    }
 }
