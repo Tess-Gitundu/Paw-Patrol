e684a1be799e0e4cc60f132870d7c42a797fdf27
Tess <njerigitundu003@gmail.com>
2022-06-13 05:01:43 +0300

    Instatiate methods and set views. Also, create a pagerAdapter class

--- app/src/main/java/com/example/gitundu/PetDetailFragment.java
+++ app/src/main/java/com/example/gitundu/PetDetailFragment.java
@@ -2,63 +2,71 @@ package com.example.gitundu;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.os.Parcel;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link PetDetailFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.example.gitundu.models.Animal;
+
+import org.parceler.Parcels;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+
 public class PetDetailFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    @BindView(R.id.petImageView) ImageView mImageLabel;
+    @BindView(R.id.petNameTextView) TextView mNameLabel;
+    @BindView(R.id.breedTextView) TextView mBreedsLabel;
+    @BindView(R.id.contactTextView) TextView mContactLabel;
+    @BindView(R.id.websiteTextView) TextView mWebsiteLabel;
+    @BindView(R.id.phoneTextView) TextView mPhoneLabel;
+    @BindView(R.id.addressTextView) TextView mAddressLabel;
+    @BindView(R.id.savePetButton) Button mSavePetButton;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private Animal mPet;
+
 
     public PetDetailFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment PetDetailFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static PetDetailFragment newInstance(String param1, String param2) {
-        PetDetailFragment fragment = new PetDetailFragment();
+    public static PetDetailFragment newInstance(Animal pet) {
+        PetDetailFragment petDetailFragment = new PetDetailFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+        args.putParcelable("pet", Parcels.wrap(pet));
+        petDetailFragment.setArguments(args);
+        return petDetailFragment;
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        assert getArguments() != null;
+        mPet = Parcels.unwrap(getArguments().getParcelable("pet"));
+
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pet_detail, container, false);
+        View view = inflater.inflate(R.layout.fragment_pet_detail, container, false);
+        ButterKnife.bind(this, view);
+        //            Picasso.get().load(pet.getPhotos().size()).into(mImageLabel);
+        mNameLabel.setText(mPet.getName());
+        mBreedsLabel.setText(mPet.getBreeds().getPrimary());
+        mContactLabel.setText(mPet.getContact().getEmail());
+        mPhoneLabel.setText(mPet.getContact().getEmail());
+        mAddressLabel.setText(mPet.getPublishedAt());
+
+        return view;
     }
 }
