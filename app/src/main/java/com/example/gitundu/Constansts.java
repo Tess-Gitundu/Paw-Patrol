c2b695a98510624ee09491fc2155b3e66cebc124
Tess <njerigitundu003@gmail.com>
2022-06-16 04:11:15 +0300

    Save user input on sharedPreferences

--- app/src/main/java/com/example/gitundu/Constansts.java
+++ app/src/main/java/com/example/gitundu/Constansts.java
@@ -3,4 +3,5 @@ package com.example.gitundu;
 public class Constansts {
     public static final String PETFINDER_BASE_URL = "https://api.petfinder.com/v2/";
     public static final String PETFINDER_API_KEY = BuildConfig.PETFINDER_API_KEY;
+    public static final String PREFERENCES_FINDER_KEY = "finder";
 }
--- app/src/main/java/com/example/gitundu/MainActivity.java
+++ app/src/main/java/com/example/gitundu/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.gitundu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,9 +15,12 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private SharedPreferences mSharedPreferences;
+    private SharedPreferences.Editor mEditor;
+
     @BindView(R.id.seeAllPetsButton) Button mSeeAllPetsButton;
     @BindView(R.id.petFinderText) EditText mPetFinderText;
-    @BindView(R.id.signUpButton) Button mSignUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,19 +28,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        mEditor = mSharedPreferences.edit();
+
         mSeeAllPetsButton.setOnClickListener(this);
-        mSignUpButton.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
         if (view == mSeeAllPetsButton) {
             String finder = mPetFinderText.getText().toString();
+            addToSharedPreferences(finder);
             Intent intent = new Intent(MainActivity.this, PetsActivity.class);
-            intent.putExtra("finder", finder);
             startActivity(intent);
-            Toast.makeText(MainActivity.this, "Jumping to all pets", Toast.LENGTH_LONG).show();
         }
     }
 
+    private void addToSharedPreferences(String finder) {
+        mEditor.putString(Constansts.PREFERENCES_FINDER_KEY, finder).apply();
+    }
 }
