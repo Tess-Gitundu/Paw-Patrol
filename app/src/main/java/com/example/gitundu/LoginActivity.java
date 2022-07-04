33a5e8954acb385951c48f56892ce66f0032366f
Tess <njerigitundu003@gmail.com>
2022-07-05 00:00:59 +0300

    Allow users to log in

--- app/src/main/java/com/example/gitundu/LoginActivity.java
+++ app/src/main/java/com/example/gitundu/LoginActivity.java
@@ -1,26 +1,43 @@
 package com.example.gitundu;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 //import com.example.gitundu.adapters.LoginAdapter;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
+    public static final String TAG = LoginActivity.class.getSimpleName();
+
     @BindView(R.id.register) TextView mRegisterAccount;
+    @BindView(R.id.email) EditText mUserEmail;
+    @BindView(R.id.pass) EditText mUserPassword;
+    @BindView(R.id.loginButton) Button mLoginButton;
+    @BindView(R.id.firebaseProgressBar) ProgressBar mSignInProgressBar;
+    @BindView(R.id.loadingTextView) TextView mLoadingSignUp;
 
-//    @BindView(R.id.tabLayout) TabLayout mTabLayout;
-//    @BindView(R.id.view_pager) ViewPager mViewPager;
-
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,41 +46,23 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         ButterKnife.bind(this);
 
         mRegisterAccount.setOnClickListener(this);
+        mLoginButton.setOnClickListener(this);
 
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
 
-//        TabLayout.Tab firstTab = mTabLayout.newTab();
-//        firstTab.setText("Log In");
-//        mTabLayout.addTab(firstTab);
-//
-//        TabLayout.Tab secondTab = mTabLayout.newTab();
-//        secondTab.setText("Create Account");
-//        mTabLayout.addTab(secondTab);
-//
-//        mTabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
-//
-//        LoginAdapter adapter = new LoginAdapter(getSupportFragmentManager(), mTabLayout.getTabCount());
-//        mViewPager.setAdapter(adapter);
-//        mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(mTabLayout));
-//
-//        mTabLayout.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(400).start();
-//
-//
-//        mTabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-//            @Override
-//            public void onTabSelected(TabLayout.Tab tab) {
-//                mViewPager.setCurrentItem(tab.getPosition());
-//            }
-//
-//            @Override
-//            public void onTabUnselected(TabLayout.Tab tab) {
-//
-//            }
-//
-//            @Override
-//            public void onTabReselected(TabLayout.Tab tab) {
-//
-//            }
-//        });
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+
     }
 
     @Override
@@ -73,5 +72,64 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             startActivity(intent);
             finish();
         }
+        if (view == mLoginButton) {
+            loginWithPassword();
+            showProgressBar();
+        }
+    }
+
+    private void loginWithPassword() {
+        String email = mUserEmail.getText().toString().trim();
+        String password = mUserPassword.getText().toString().trim();
+        if (email.equals("")) {
+            mUserEmail.setError("Please enter your email");
+            return;
+        }
+        if (password.equals("")) {
+            mUserPassword.setError("Password cannot be blank");
+            return;
+        }
+//        mAuthProgressDialog.show();
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        hideProgressBar();
+                        Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithEmail", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    private void showProgressBar() {
+        mSignInProgressBar.setVisibility(View.VISIBLE);
+        mLoadingSignUp.setVisibility(View.VISIBLE);
+//        mAuthProgressDialog.setMessage("Authenticating with Firebase...");
+        mLoadingSignUp.setText("Logging you in");
+    }
+
+    private void hideProgressBar() {
+        mSignInProgressBar.setVisibility(View.GONE);
+        mLoadingSignUp.setVisibility(View.GONE);
     }
 }
