a9297d92a026958b4f105bb7684ab87ff26d2a5e
Tess <njerigitundu003@gmail.com>
2022-07-05 03:49:19 +0300

    Create FirebasePetListAdapter class that allows drag and drop functionalities

--- app/src/main/java/com/example/gitundu/adapters/FirebasePetListAdapter.java
+++ app/src/main/java/com/example/gitundu/adapters/FirebasePetListAdapter.java
@@ -1,4 +1,51 @@
 package com.example.gitundu.adapters;
 
-public class FirebasePetListAdapter {
+import android.content.Context;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+
+import com.example.gitundu.models.Animal;
+import com.example.gitundu.utils.ItemTouchHelperAdapter;
+import com.example.gitundu.utils.OnStartDragListener;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+
+public class FirebasePetListAdapter extends FirebaseRecyclerAdapter<Animal, FirebasePetViewHolder> implements ItemTouchHelperAdapter {
+
+    private DatabaseReference mRef;
+    private OnStartDragListener mOnStartDragListener;
+    private Context mContext;
+
+    public FirebasePetListAdapter(FirebaseRecyclerOptions<Animal> options,
+                                  DatabaseReference ref,
+                                  OnStartDragListener onStartDragListener,
+                                  Context context){
+        super(options);
+        mRef = ref.getRef();
+        mOnStartDragListener = onStartDragListener;
+        mContext = context;
+    }
+
+    @Override
+    public boolean onItemMove(int fromPosition, int toPosition) {
+        return false;
+    }
+
+    @Override
+    public void onItemDismiss(int position) {
+
+    }
+
+    @Override
+    protected void onBindViewHolder(@NonNull FirebasePetViewHolder holder, int position, @NonNull Animal model) {
+
+    }
+
+    @NonNull
+    @Override
+    public FirebasePetViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
 }
